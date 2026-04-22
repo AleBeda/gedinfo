df19c78b60252b99a0e2688f05a6cf6ae5b5b1ee
Ale Beda <96376540+AleBeda@users.noreply.github.com>
2026-04-22 17:19:39 +0200

    fix: update version assertion in test_cli to 0.5.0

--- tests/test_commands/test_cli.py
+++ tests/test_commands/test_cli.py
@@ -28,7 +28,7 @@ def test_cli_version(monkeypatch, capsys):
         ["--version", "name", "@I001@", str(FIXTURES / "simple.ged")],
     )
     assert code == 0
-    assert "gedinfo 0.4.1" in out
+    assert "gedinfo 0.5.0" in out
     # ensure subcommand isn't executed when version flag is present
     assert "John Smith" not in out
 
